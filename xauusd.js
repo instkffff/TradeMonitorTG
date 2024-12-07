c9627c204ff817953794e60786f45e9220821aa1
NightCandle <instkffff@gmail.com>
2024-12-07 11:49:55 +0800

    .

--- xauusd.js
+++ xauusd.js
@@ -33,43 +33,34 @@ Today Trends: ${result.TodayTrends}
     bot.telegram.sendMessage(process.env.CHANNEL_ID, message, { parse_mode: 'Markdown' });
 }
 
-let xauusdEnabled = true;
-let crudeEnabled = true;
+let xauusdStatus = true;
+let crudeStatus = true;
 
-setInterval(async () => {
-    if (xauusdEnabled) {
-        try {
-            let result = await fetchData();
-            XAUUSD(result);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-            bot.telegram.sendMessage(process.env.CHANNEL_ID, 'Error fetching data: ' + error.message);
-        }
-    }
-}, 1000 * 60 * 10); // 每10分钟执行一次
-
-// 启动第二个定时任务，延迟5分钟后开始
-setTimeout(() => {
-    setInterval(async () => {
-        if (crudeEnabled) {
+function SendTimer(func, interval, delay, status) {
+    setTimeout(() => {
+        setInterval(async () => {
+            if (!status()) return; // 使用状态函数来检查是否执行
             try {
-                let result = await getCrudeOilFuturesPrice();
-                WTICRUDE(result);
+                let result = await func();
+                func(result);
             } catch (error) {
                 console.error('Error fetching data:', error);
                 bot.telegram.sendMessage(process.env.CHANNEL_ID, 'Error fetching data: ' + error.message);
             }
-        }
-    }, 1000 * 60 * 10); // 每10分钟执行一次
-}, 1000 * 60 * 5); // 延迟5分钟启动
+        }, interval);
+    }, delay);
+}
+
+SendTimer(() => fetchData().then(XAUUSD), 1000 * 60 * 10, 0, () => xauusdStatus); // 每10分钟执行一次，立即启动
+SendTimer(() => getCrudeOilFuturesPrice().then(WTICRUDE), 1000 * 60 * 10, 1000 * 60 * 5, () => crudeStatus); // 每10分钟执行一次，延迟5分钟启动
 
 bot.command('XAUUSD', (ctx) => {
     const arg = ctx.message.text.split(' ')[1];
     if (arg === '0') {
-        xauusdEnabled = false;
+        xauusdStatus = false;
         ctx.reply('XAUUSD messages disabled.');
     } else if (arg === '1') {
-        xauusdEnabled = true;
+        xauusdStatus = true;
         ctx.reply('XAUUSD messages enabled.');
     } else {
         ctx.reply('Invalid argument. Use /XAUUSD 0 to disable or /XAUUSD 1 to enable.');
@@ -79,10 +70,10 @@ bot.command('XAUUSD', (ctx) => {
 bot.command('CRUDE', (ctx) => {
     const arg = ctx.message.text.split(' ')[1];
     if (arg === '0') {
-        crudeEnabled = false;
+        crudeStatus = false;
         ctx.reply('CRUDE messages disabled.');
     } else if (arg === '1') {
-        crudeEnabled = true;
+        crudeStatus = true;
         ctx.reply('CRUDE messages enabled.');
     } else {
         ctx.reply('Invalid argument. Use /CRUDE 0 to disable or /CRUDE 1 to enable.');
